1262b065ee495de58fbe404a2b7af9d5a153d946
기수 <87399695+su2333066@users.noreply.github.com>
2023-01-17 12:27:39 +0900

    IMG: Fix

--- main.js
+++ main.js
@@ -52,6 +52,24 @@ arrowUp.addEventListener("click", () => {
   scrollIntoViews("#home");
 });
 
+const workCategories = document.querySelector(".work__categories");
+const workProjects = document.querySelectorAll(".project");
+workCategories.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  workProjects.forEach((project) => {
+    if (project.dataset.link === link) {
+      project.style.display = "flex";
+    } else {
+      project.style.display = "none";
+    }
+  });
+});
+
 function scrollIntoViews(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
